b255dbff461c4296707c19c1b01b699237e88617
Syed Hussain <syed.m.husyn@gmail.com>
2020-09-02 10:09:27 +1200

    adding referencing to other container and adding restart policy

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const redis = require('redis');
 
 const app = express();
 
-const client = redis.createClient();
+const client = redis.createClient({
+    //this redis-server is going to be replaced/redirected to the redis-server container
+    host: 'redis-server',
+    port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+    process.exit(0);
     client.get('visits', (error, visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
